482c03633d0a296187e7be8ad3b85d9790f81324
keane <keaneathallah960@gmail.com>
2023-10-27 16:13:55 +0800

    memnyiap UI

---
+++ resources/views/frontend/partsials/footer.blade.php
@@ -0,0 +1,71 @@
+    <!-- Footer Start -->
+    <div class="container-fluid bg-secondary text-light footer wow fadeIn mt-5 pt-5" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-uppercase mb-4">Get In Touch</h4>
+                    <div class="d-flex align-items-center mb-2">
+                        <div class="btn-square bg-dark me-3 flex-shrink-0">
+                            <span class="fa fa-map-marker-alt text-primary"></span>
+                        </div>
+                        <span>123 Street, New York, USA</span>
+                    </div>
+                    <div class="d-flex align-items-center mb-2">
+                        <div class="btn-square bg-dark me-3 flex-shrink-0">
+                            <span class="fa fa-phone-alt text-primary"></span>
+                        </div>
+                        <span>+012 345 67890</span>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <div class="btn-square bg-dark me-3 flex-shrink-0">
+                            <span class="fa fa-envelope-open text-primary"></span>
+                        </div>
+                        <span>info@example.com</span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-uppercase mb-4">Quick Links</h4>
+                    <a class="btn btn-link" href="">About Us</a>
+                    <a class="btn btn-link" href="">Contact Us</a>
+                    <a class="btn btn-link" href="">Our Services</a>
+                    <a class="btn btn-link" href="">Terms & Condition</a>
+                    <a class="btn btn-link" href="">Support</a>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-uppercase mb-4">Newsletter</h4>
+                    <div class="position-relative mb-4">
+                        <input class="form-control w-100 border-0 py-3 pe-5 ps-4" type="text"
+                            placeholder="Your email">
+                        <button type="button"
+                            class="btn btn-primary position-absolute end-0 top-0 me-2 mt-2 py-2">SignUp</button>
+                    </div>
+                    <div class="d-flex m-n1 pt-1">
+                        <a class="btn btn-lg-square btn-dark text-primary m-1" href=""><i
+                                class="fab fa-twitter"></i></a>
+                        <a class="btn btn-lg-square btn-dark text-primary m-1" href=""><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-lg-square btn-dark text-primary m-1" href=""><i
+                                class="fab fa-youtube"></i></a>
+                        <a class="btn btn-lg-square btn-dark text-primary m-1" href=""><i
+                                class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-md-start mb-md-0 mb-3 text-center">
+                        &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-md-end text-center">
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a>
+                        <br>Distributed By: <a class="border-bottom" href="https://themewagon.com"
+                            target="_blank">ThemeWagon</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
